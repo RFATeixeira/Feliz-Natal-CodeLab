ae249fc5bb20e4ea6f1c7e1138070b461ef43e62
RFATeixeira <RCSTeixeira@hotmail.com>
2024-08-02 20:37:26 -0300

    add Timer

---
+++ src/script.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const endCounter = "12 aug 2024";
+
+function countDown() {
+  const endCounterDate = new Date(endCounter);
+  const currentDate = new Date();
+
+  const totalSeconds = (endCounterDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(mins);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
