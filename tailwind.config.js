55594c498fa45e7b1a48e905fe353faaa3be04eb
RFATeixeira <RCSTeixeira@hotmail.com>
2024-08-02 20:01:54 -0300

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-red-10": "#CD3C32",
+        "brand-red-20": "#FFE9E8",
+        "dark-color": "#121214",
+      },
+    },
+    fontFamily: {
+      body: ["Archivo", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
